ab0c2282acc4799fc73a02f59f71629f325b2087
qm210 <quantenmace@gmail.com>
2025-03-05 02:01:03 +0100

    refactor structure

---
+++ src/showcases/2_HelloShadertoy.js
@@ -0,0 +1,7 @@
+import vertexShaderSource from "./shaders/basicVertex.glsl";
+import fragment1 from "./shaders/fragment1.glsl";
+import fragment2 from "./shaders/fragment2.glsl";
+import fragmentShadertoyAsIs from "./shaders/fragmentShadertoy.glsl";
+
+// choose wisely :)
+const fragmentShaderSource = fragmentShadertoyAsIs;
